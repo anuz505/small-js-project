facc32eace3a85ddb8c38b4294e57ff9d4191c3a
anuj bhandari <anujb364@gmail.com>
2024-03-04 14:42:35 +0545

    new commit

--- script.js
+++ script.js
@@ -17,11 +17,13 @@ function changeimageno(){
     var imageC = document.querySelector('img');
     var randomIndex = Math.floor(Math.random()*imagenoUrls.length);
     imageC.src= imagenoUrls[randomIndex];
+    document.querySelector('.go').innerHTML = '<h1>plsssssssssssss</h1> '
 }
 function changeimageyes(){
     var imageY = document.querySelector('img');
     imageY.src="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZm82Y3ViMnhndWRnejE1a2JxNThicjNwNmw1MGFiMHdkaTltNWY0aSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/7zGmpJmwUp5lNFudOf/giphy.gif" ;
-    document.querySelector('.go').innerHTML = '<h1>Awwww</h1>'
+    document.querySelector('.go').innerHTML = '<h1>Awwww, i knew you would accept.</h1>'
     var button = document.querySelector('.no');
     button.style.position = 'static';
+   
 }
